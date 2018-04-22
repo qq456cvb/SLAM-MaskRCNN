35503dbcd10204347105b018d1f0ed989f41395f
Neil You <neilyou@qq.com>
2018-04-22 14:14:17 +0800

    clean files

--- SfM_CUDA/configuration.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Configuration {
-public:
-	static float prior_mrcnn_err_rate;
-	static float duplicate_thresh;
-};
-
-float Configuration::prior_mrcnn_err_rate = 0.05f;
-float Configuration::duplicate_thresh = 0.5f;
